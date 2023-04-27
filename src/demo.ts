319195f45199bea450676c92233c406ca07a62f8
Luis Daniel Mendez Castellanos <syi216618@gmail.com>
2023-04-27 15:36:56 -0600

    Se agregaron comentarios al codigo

--- src/demo.ts
+++ src/demo.ts
@@ -1,6 +1,9 @@
+//Importaciones necesarias para el desarrollo del mapeo
 import { near, BigInt, log, BigDecimal, JSONValue, JSONValueKind, json } from "@graphprotocol/graph-ts";
+//Importacion de los esquemas que hemos generado
 import { Profile, Book, Sale } from "../generated/schema";
 
+//Mapeo de la informacion mediante el receipt
 export function handleReceipt(
   receipt: near.ReceiptWithOutcome
 ): void {
@@ -16,6 +19,7 @@ export function handleReceipt(
   }
 }
 
+//Declaracion de la funcion para realizar el mapeo de la informacion de la blockchain
 function handleAction(
   action: near.ActionValue,
   receipt: near.ActionReceipt,
@@ -23,24 +27,35 @@ function handleAction(
   outcome: near.ExecutionOutcome,
   publicKey: near.PublicKey
 ): void {
+  //Si la accion no es function call cancelamos la ejecucion
   if (action.kind !== near.ActionKind.FUNCTION_CALL) {
     log.info("Early return: {}", ["Not a function call"]);
     return;
   }
 
+  //Recuperamos la funcion ejecutada
   const functionCall = action.toFunctionCall();
 
+  //Mapeo de la funcion create_profile
   if(functionCall.methodName == "create_profile"){
     //log.warning("Log:{}",[outcome.logs[0]])
+    //Se realiza la declaracion de las variables que se van a recuperar
     let name = ""
     let bio = ""
+    //Se recupera el log que se tiene en la transaccion de la blockchain
     let jsonData = outcome.logs[0]
+    //Se convierte el log del estandar de eventos a JSON
     let parsedJSON = json.fromString(jsonData.replace("EVENT_JSON:", ""));
+    //El JSON se convierte a objeto
     let entry = parsedJSON.toObject();
+    //Extraemos el campo data del objeto del estandar de eventos
     let data = entry.entries[0].value.toObject();
+    //Se declara un ciclo for para poder extraer la informacion
     for (let i = 0; i < data.entries.length; i++) {
+      //Se declara la variable key para poder extraer el encabezado del objeto
       let key = data.entries[i].key.toString();
       //log.warning("Key:{} Pos:{}",[data.entries[i].key.toString(),i.toString()])
+      //Se declara una estructura Switch para almacenar la informacion en sus correspondientes variables
       switch (true) {
         case key == "bio":
           bio = data.entries[i].value.toString();
@@ -50,13 +65,17 @@ function handleAction(
           break;
       }
     }
+    //Se realiza la carga del perfil del usuario con la persona que firmo la transaccion
     let profile = Profile.load(receipt.signerId)
+    //Si este perfil no existe se genera uno nuevo
     if(profile == null){
       profile = new Profile(receipt.signerId)
     }
+    //Se asignan los valores que se recuperaron del log
     profile.bio = bio
     profile.name = name
     profile.timestamp = BigInt.fromString(blockHeader.timestampNanosec.toString())
+    //Se guardan los cambios realizados en la estructura
     profile.save()
   }
 
@@ -104,6 +123,7 @@ function handleAction(
     }
     book.author = author
     book.description = description
+    //Se declara el valor BigDecimal mediante el string declarado anteriormente
     book.price = BigDecimal.fromString(price)
     book.stock = stock
     book.title = title
@@ -136,6 +156,7 @@ function handleAction(
     }
     book.stock = BigInt.fromString((book.stock - BigInt.fromI32(1)).toString())
     let sale = new Sale(receipt.id.toHexString())
+    //Se asignan los valores de la relacion mediante el string obtenido con anterioridad
     sale.book = id
     sale.price = book.price
     sale.owner = owner
